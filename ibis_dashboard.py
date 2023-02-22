05cd1ba0a6044761fb6864e6989e5ebad1e336ad
Bryce Mecum <petridish@gmail.com>
2023-02-21 19:50:11 -0900

    Initial commit

---
+++ ibis_dashboard.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+RELEASES_URL = "https://api.github.com/repos/ibis-project/ibis/releases"
+
+
+@st.cache_data
+def load_data():
+    data = pd.read_json(RELEASES_URL)
+    return data
+
+
+st.title("Ibis Releases Dashboard")
+data = load_data()
+st.subheader("Releases")
+st.write(data)
